e227bdba8a511a0087782cd300290d2ec7e9026b
sofia <mailtoosofi@gmail.com>
2016-07-07 17:41:43 +0530

    msg1

---
+++ HQL_HIBER/src/java/Data.java
@@ -0,0 +1,38 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MRuser
+ */
+@Entity
+public class Data {
+    @Id
+    private int did;
+    private String dname;
+
+    public int getDid() {
+        return did;
+    }
+
+    public void setDid(int did) {
+        this.did = did;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+    
+    
+}
